163f3df0672c1cca74c9a254c15923ee6b8ff117
lehogy <m16_a1@naver.com>
2023-02-03 16:31:21 +0900

    중간 저장

--- routes/pages.routes.js
+++ routes/pages.routes.js
@@ -16,24 +16,30 @@ router.get('/signup', (req, res) => {
 //
 
 //이호균 page
-const { User, /*Items*/ } = require('../models') // User 테이블 연결
+const { user, /*Items*/ } = require('../models') // User 테이블 연결 // ※※※※※※※※※※모델명 변경예정※※※※※※※※※※※※※※
 const authMiddleware = require('../middlewares/auth.middleware') // 미들웨어 연결
 
-router.get('/', /*authMiddleware,*/ async (req, res) => {
-  const {user} = res.locals // 로그인된 유저를 가져옵니다.
+// 마이페이지 조회
+router.get('/mypage', /*authMiddleware,*/ async (req, res) => {
+  const {loginUser} = res.locals // 로그인된 유저를 가져옵니다.
 
-  // res.cookie('cart','고양이 후드') // 테스트
+  res.cookie('cart','고양이 후드') // 테스트
+  res.cookie('cart2','멍멍이 후드') // 테스트
+  res.cookie('cart3','알파카 간식') // 테스트
   // console.log(req.cookies) // 테스트
 
   try {
     // 유저의 user_idx를 기준으로 정보 목록을 가져옵니다.
-    const userInfo = await User.findByPk(1/*user.user_idx*/)
+    const userInfo = await user.findByPk(1/*loginuser.user_idx*/) // ※※※※※※※※※※모델명 변경예정※※※※※※※※※※※※※※
     
     // 장바구니 정보 목록을 가져옵니다. (유저의 쿠키에서 장바구니 정보를 가져와야함)
-    const userProduct = !req.cookies.cart ? '장바구니가 비었습니다.' : req.cookies.cart
+    // const userProduct = !req.cookies.cart ? '장바구니가 비었습니다.' : req.cookies.cart
     
+    const cartProduct = req.cookies.cart
+    const cartEmpty = '장바구니가 비었습니다.'
+
     // ejs로 정보를 보냅니다.
-    res.render('my_page', { info: { userInfo }, product: { userProduct } })
+    res.render('my_page', { info: { userInfo }, cart: { cartProduct, cartEmpty } })
 
     // 유저 정보가 없으면 로그인 메세지를 보냅니다.
     if (!userInfo) {
@@ -43,6 +49,38 @@ router.get('/', /*authMiddleware,*/ async (req, res) => {
     res.status(500).json({ message: error.message })
   }
 })
+
+// 마이페이지 회원정보(비밀번호, 주소) 수정 
+router.patch('/mypage', /*authMiddleware,*/async (req, res) => {
+  const {loginUser} = res.locals // 로그인된 유저를 가져옵니다.
+  
+  const { user_password, user_address } = req.body
+  console.log(user_password)
+  console.log(user_address)
+  const userInfo = await user.findByPk(1/*loginuser.user_idx*/) // ※※※※※※※※※※모델명 변경예정※※※※※※※※※※※※※※
+  console.log(userInfo)  
+  if (userInfo) {
+    if (user_password) {
+      userInfo.user_password = user_password
+    }
+    if (user_address) {
+      userInfo.user_address = user_address
+    }
+  }
+
+  try {
+    await userInfo.save()
+    res.json({message: '수정 완료'})
+  } catch (error) {
+    res.status(500).json({ message: error.message })
+  }
+})
+
+// router.delete('/page/mypage', async (req, res) => {
+//   console.log({data})
+//   res.json({ message: '연결 완료' })
+// })
+
 //
 
 //이설인 page
