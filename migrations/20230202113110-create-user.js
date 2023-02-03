3b1df80de3ee32de61d4d66553a98fb4c8044546
ByunJungMin <cvy5375@naver.com>
2023-02-03 14:51:56 +0900

    user_type 변경

--- migrations/20230202113110-create-user.js
+++ migrations/20230202113110-create-user.js
@@ -2,7 +2,7 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('Users', {
+    await queryInterface.createTable('users', {
       user_idx: {
         allowNull: false,
         autoIncrement: true,
@@ -41,6 +41,6 @@ module.exports = {
     });
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('Users');
+    await queryInterface.dropTable('users');
   }
 };
