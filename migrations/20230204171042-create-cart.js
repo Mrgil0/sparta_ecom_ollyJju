e05ec6b6a3557c1ad99dfc9c33144418af567bff
lehogy <m16_a1@naver.com>
2023-02-05 02:40:37 +0900

    밤

---
+++ migrations/20230204171042-create-cart.js
@@ -0,0 +1,34 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('carts', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      product_idx: {
+        type: Sequelize.INTEGER
+      },
+      user_email: {
+        type: Sequelize.STRING
+      },
+      count: {
+        type: Sequelize.INTEGER
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('carts');
+  }
+};
