5d220c7f35a5e7cfff823745e186487d24a670cf
ByunJungMin <cvy5375@naver.com>
2023-02-07 04:19:02 +0900

    feat: my_page css/js 분리

--- routes/app.js
+++ routes/app.js
@@ -42,7 +42,6 @@ app.get("/home", authMiddleware, async (req, res) => {
 });
 
 app.get('/product_detail', authMiddleware, async (req, res) => {
-  const chatRepository = new ChatRepository();
   const user = res.locals.user;
   const room = await chatRepository.findAllRoom()
   const chat = await chatRepository.findAllChat(user.user_email);
