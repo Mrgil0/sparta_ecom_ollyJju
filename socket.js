a22a156760a8a9722bf56b1298af6e58136738bc
Mrgil <dieoxin4@naver.com>
2023-02-08 14:38:06 +0900

    소켓수정

--- socket.js
+++ socket.js
@@ -23,22 +23,11 @@ io.on("connection", (socket) =>{
 		let enterUser = await room.findOne({
 			where: {user_key: user_key}
 		})
-		console.log('찾은 유저' + enterUser);
 		if(!enterUser){
 			enterUser = await room.create({user_key: user_key})
 		} 
-		console.log('유저 생성' + enterUser)
 		socket.join(enterUser.room_key);
-		console.log('방 생성')
 	})
-	// socket.on('guest_chat_message', async (data) =>{
-	// 	let { message, user_key} = data;
-	// 	const enterUser = await room.findOne({
-	// 		where: {user_key: user_key}
-	// 	})
-	// 	const newChat = await chat.create({room_key: enterUser.room_key, chat_person: user_key, message: message, check:0})
-	// 	io.to(enterUser.room_key).emit('message', newChat)
-	// })
 	socket.on('chat_message', async (data) =>{
 		let { message, user_key, room_key } = data;
 		let newChat = []
@@ -50,12 +39,7 @@ io.on("connection", (socket) =>{
 			})
 			newChat = await chat.create({room_key: enterUser.room_key, chat_person: user_key, message: message, check:0})
 		}
-		console.log(newChat.message)
 		io.in(newChat.room_key).emit('message', newChat)
-	})
-
-	socket.on('send_msg', function(data){
-		console.log(data);
 	})
 
 	socket.on("disconnect", () => {
